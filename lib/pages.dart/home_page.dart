5ce287c1ba843bde80ec0109a6cf35576ca18b45
kkong7 <kkongk@naver.com>
2025-01-18 11:50:49 +0900

    appBar추가

--- lib/pages.dart/home_page.dart
+++ lib/pages.dart/home_page.dart
@@ -5,6 +5,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Koko Habit Tracker'),
+      ),
+    );
   }
 }
